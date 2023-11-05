0161d1a0ecc76f8b32a06382ac3dcdd68beed34f
chuong_gamer <hieutg244@gmail.com>
2023-11-05 15:36:01 +0700

    update

---
+++ Assets/Script/House/ComingSoonHouse.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComingSoonHouse : MonoBehaviour
+{
+private bool isRunIE;
+    private bool overlap;
+    private bool dragging;
+    private Vector3 oldPos;
+    private Vector3 camfirstPos;
+    private Animator Ani;
+    private Rigidbody2D rgb2D;
+    private SpriteRenderer sprRenderer;
+    private SpriteRenderer sprShadow;
+    [SerializeField] GameObject Store;
+    [SerializeField] GameObject Shadow;
+    // Use this for initialization
+    void Start()
+    {
+        Ani = GetComponent<Animator>();
+        sprRenderer = Store.GetComponent<SpriteRenderer>();
+        sprShadow = Shadow.GetComponent<SpriteRenderer>();
+        float order = this.transform.position.y * (-100);
+        sprRenderer.sortingOrder = (int)order;
+        sprShadow.sortingOrder = (int)order - 1;
+    }
+
+    void OnMouseDown()
+    {
+        sprRenderer.color = new Color(0.3f, 0.3f, 0.3f, 1f);
+        camfirstPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnMouseDrag()
+    {
+        if (Vector3.Distance(camfirstPos, Camera.main.ScreenToWorldPoint(Input.mousePosition)) >= 0.2f)
+        {
+            if (sprRenderer.color != Color.white) sprRenderer.color = Color.white;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (Vector3.Distance(camfirstPos, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < 0.2f)
+        {
+            Ani.SetTrigger("isClick");
+            sprRenderer.color = Color.white;
+            string str;
+            if (Application.systemLanguage == SystemLanguage.Vietnamese)
+                str = "Sắp có!";
+            else if (Application.systemLanguage == SystemLanguage.Indonesian)
+                str = "Segera akan datang!";
+            else str = "Coming soon!";
+            Notification.instance.dialogBelow(str);
+        }
+    }
+
+    public void onTriggerStay2D()
+    {
+        if (dragging == true)
+        {
+            overlap = true;
+            sprRenderer.color = new Color(1f, 0f, 0f, 1f);
+        }
+    }
+
+    public void onTriggerExit2D()
+    {
+        if (dragging == true)
+        {
+            overlap = false;
+            sprRenderer.color = Color.white;
+        }
+    }
+}
