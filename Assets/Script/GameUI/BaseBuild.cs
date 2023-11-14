79455fecc734780bbd78ea8bb78b1bee2dc0d416
chuong_gamer <hieutg244@gmail.com>
2023-11-14 07:58:03 +0700

    update

--- Assets/Script/GameUI/BaseBuild.cs
+++ Assets/Script/GameUI/BaseBuild.cs
@@ -1,42 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace NongTrai
 {
     public abstract class BaseBuild : MonoBehaviour
     {
-        Vector3 _camfirstPos;
-        Animator _ani;
-        protected float Order;
-
         [SerializeField] SpriteRenderer sprRenderer;
 
-        // Use this for initialization
+        private bool _overlap;
+        protected bool dragging;
+        protected Vector3 oldPos;
+        private Vector3 _camfirstPos;
+        protected Rigidbody2D rgb2D;
+        protected SpriteRenderer _sprRenderer;
+
+        [FormerlySerializedAs("Store")] [SerializeField]
+        protected GameObject store;
+
+        public BaseBuild(bool isRunIE, bool dragging, Vector3 oldPos, Rigidbody2D rgb2D)
+        {
+            this.dragging = dragging;
+            this.oldPos = oldPos;
+            this.rgb2D = rgb2D;
+        }
+
         protected virtual void Start()
         {
-            _ani = this.GetComponent<Animator>();
-            Order = this.transform.position.y * (-100);
-            sprRenderer.sortingOrder = (int) Order;
+            _sprRenderer = store.GetComponent<SpriteRenderer>();
+            float order = this.transform.position.y * (-100);
+            _sprRenderer.sortingOrder = (int) order;
         }
 
         void OnMouseDown()
         {
-            sprRenderer.color = new Color(0.3f, 0.3f, 0.3f, 1f);
-            if (Camera.main != null) _camfirstPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _sprRenderer.color = new Color(0.3f, 0.3f, 0.3f, 1f);
+            _camfirstPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
         void OnMouseDrag()
         {
-            if (Camera.main == null ||
+            if (Camera.main != null &&
                 !(Vector3.Distance(_camfirstPos, Camera.main.ScreenToWorldPoint(Input.mousePosition)) >= 0.2f)) return;
-            sprRenderer.color = Color.white;
+            if (_sprRenderer.color != Color.white) _sprRenderer.color = Color.white;
         }
 
         void OnMouseUp()
         {
+            Debug.LogError("Choose ho cai");
+            
             if (Camera.main == null ||
                 !(Vector3.Distance(_camfirstPos, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < 0.2f)) return;
-            sprRenderer.color = Color.white;
+
+            _sprRenderer.color = Color.white;
             OpenPopup();
+        }
+
+        public void onTriggerStay2D()
+        {
+            if (dragging)
+            {
+                _overlap = true;
+                _sprRenderer.color = new Color(1f, 0f, 0f, 1f);
+            }
+        }
+
+        public void onTriggerExit2D()
+        {
+            if (dragging == true)
+            {
+                _overlap = false;
+                _sprRenderer.color = Color.white;
+            }
         }
 
         protected abstract void OpenPopup();
--- Assets/Script/NameTag/NameTag.cs
+++ Assets/Script/NameTag/NameTag.cs
@@ -1,38 +1,91 @@
-using Script.GameUI;
-using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace NongTrai
 {
-    public class NameTag : BaseBuild
+    public class NameTag : MonoBehaviour
     {
         [SerializeField] private Text _nameText;
         [SerializeField] private Canvas _canvas;
 
-        private void OnEnable()
+        private bool _overlap;
+        protected bool dragging;
+        protected Vector3 oldPos;
+        private Vector3 _camfirstPos;
+        protected Rigidbody2D rgb2D;
+        protected SpriteRenderer _sprRenderer;
+
+        [FormerlySerializedAs("Store")] [SerializeField]
+        protected GameObject store;
+
+        void OnMouseDown()
+        {
+            _sprRenderer.color = new Color(0.3f, 0.3f, 0.3f, 1f);
+            _camfirstPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        void OnMouseDrag()
+        {
+            if (Camera.main != null &&
+                !(Vector3.Distance(_camfirstPos, Camera.main.ScreenToWorldPoint(Input.mousePosition)) >= 0.2f)) return;
+            if (_sprRenderer.color != Color.white) _sprRenderer.color = Color.white;
+        }
+
+        void OnMouseUp()
+        {
+            Debug.LogError("Choose ho cai");
+
+            if (Camera.main == null ||
+                !(Vector3.Distance(_camfirstPos, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < 0.2f)) return;
+
+            _sprRenderer.color = Color.white;
+            OpenPopup();
+        }
+
+        public void onTriggerStay2D()
+        {
+            if (dragging)
+            {
+                _overlap = true;
+                _sprRenderer.color = new Color(1f, 0f, 0f, 1f);
+            }
+        }
+
+        public void onTriggerExit2D()
+        {
+            if (dragging == true)
+            {
+                _overlap = false;
+                _sprRenderer.color = Color.white;
+            }
+        }
+        protected void Start()
         {
             SetName();
+            
+            _sprRenderer = store.GetComponentInChildren<SpriteRenderer>();
+            float order = this.transform.position.y * (-100);
+            _sprRenderer.sortingOrder = (int) order;
+            _canvas.sortingOrder = (int) (order + 1);
         }
-
-        protected override void Start()
-        {
-            base.Start();
-            _canvas.sortingOrder = (int) (Order + 1);
-        }
-
-        
 
         public void SetName()
         {
             _nameText.text = PlayerPrefs.GetString("NameTag", "NameTag");
         }
-        
-        
-        [Button]
-        protected override void OpenPopup()
+
+        protected void OpenPopup()
         {
             NameTagManager.Instance.OpenPopup();
         }
+
+        public NameTag(bool dragging, Vector3 oldPos, Rigidbody2D rgb2D)
+        {
+            this.dragging = dragging;
+            this.oldPos = oldPos;
+            this.rgb2D = rgb2D;
+        }
+
     }
 }
